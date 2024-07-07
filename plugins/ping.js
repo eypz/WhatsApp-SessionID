a31b70a154428f97e47c84035d0783f05cb52cff
A7-T62 <109889758+Diegoson@users.noreply.github.com>
2024-07-07 10:46:12 +0200

    😂

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,14 +1,15 @@
 
-module.exports = function(bot) {
-
-  bot.onText(/\/ping/, async (msg) => {
+module.exports = {
+  command: '/ping',
+  description: 'Speed',
+  handler: async function(bot, msg) {
     const chatId = msg.chat.id;
     const startTime = Date.now();
     const sentMessage = await bot.sendMessage(chatId, 'Pinging...');
     const endTime = Date.now();
     const Time = endTime - startTime;
 
-    await bot.editMessageText(`Response time:${Time}`, {
+    await bot.editMessageText(`Speed:${Time}`, {
       chat_id: chatId,
       message_id: sentMessage.message_id
     });
